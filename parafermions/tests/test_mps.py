897ec2fedd481bc59c7f7c0e5093dedae97215e5
Niall Moran <nmoran@cartrawler.com>
2018-05-02 23:51:56 +0100

    Update to get first unittest working.

--- parafermions/tests/test_mps.py
+++ parafermions/tests/test_mps.py
@@ -3,16 +3,21 @@
 """
 Test the MPS class
 """
-import parafermions as pf
-
+import unittest
 import numpy as np
 
-def test_mps_initialisation():
-    # should initialise with all zeros
-    mps = pf.MPS(4, 4, 2)
-    assert(np.sum([np.sum(x) for i, x in mps.M.items()]) == 4.0)
+import parafermions as pf
 
-    mps = pf.MPS(4, 4, 2, dtype=np.complex128)
-    assert(np.sum([np.sum(x) for i, x in mps.M.items()]) == 4.0)
+class Test(unittest.TestCase):
+
+    def test_mps_initialisation(self):
+        # should initialise with all zeros
+        mps = pf.MPS(4, 4, 2)
+        assert(np.sum([np.sum(x) for i, x in mps.M.items()]) == 4.0)
+
+        mps = pf.MPS(4, 4, 2, dtype=np.complex128)
+        assert(np.sum([np.sum(x) for i, x in mps.M.items()]) == 4.0)
 
 
+    def test_foo(self):
+        assert(True)
