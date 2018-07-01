2bb45d24291bd7deeb7e53711b8b3cff1c2b5a1e
Niall (Carcosa) <niall.moran@gmail.com>
2018-07-01 14:06:10 +0100

    Update path to class

--- parafermions/tests/test_peschel_emery.py
+++ parafermions/tests/test_peschel_emery.py
@@ -13,12 +13,12 @@ class Test(unittest.TestCase):
     def test_pe_degeneracy(self):
         # should initialise with all zeros
         N, l = 8, 0.2
-        pe = PeschelEmerySpinHalf(N, l, dtype=np.dtype('float64'))
+        pe = pf.PeschelEmerySpinHalf(N, l, dtype=np.dtype('float64'))
         d, v = pe.Diagonalise(k=100)
         assert(np.sum(d[1:11:2]-d[:11:2]) < 1e-10)
 
         N, l = 8, 1.0
-        pe = PeschelEmerySpinHalf(N, l, dtype=np.dtype('float64'))
+        pe = pf.PeschelEmerySpinHalf(N, l, dtype=np.dtype('float64'))
         d, v = pe.Diagonalise(k=100)
         assert((d[1]-d[0]) < 1e-15)
         assert(np.sum(d[1:11:2]-d[:11:2]) > 1e-2)
